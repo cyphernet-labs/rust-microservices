8fc95d98401b619bc770d7432a3b88a197ede895
Dr. Maxim Orlovsky <orlovsky@pandoraprime.ch>
2022-06-21 22:23:42 +0200

    File utilities

--- src/cli.rs
+++ src/cli.rs
@@ -11,6 +11,10 @@
 // along with this software.
 // If not, see <https://opensource.org/licenses/MIT>.
 
+use std::io::{Read, Write};
+use std::path::Path;
+use std::{fs, io};
+
 use colored::Colorize;
 
 pub trait LogStyle: ToString {
@@ -36,3 +40,40 @@ pub trait LogStyle: ToString {
 }
 
 impl<T> LogStyle for T where T: ToString {}
+
+pub fn open_file_or_stdin(filename: Option<impl AsRef<Path>>) -> Result<Box<dyn Read>, io::Error> {
+    Ok(match filename {
+        Some(filename) => {
+            let file = fs::File::open(filename)?;
+            Box::new(file)
+        }
+        None => Box::new(io::stdin()),
+    })
+}
+
+pub fn open_file_or_stdout(
+    filename: Option<impl AsRef<Path>>,
+) -> Result<Box<dyn Write>, io::Error> {
+    Ok(match filename {
+        Some(filename) => {
+            let file = fs::File::create(filename)?;
+            Box::new(file)
+        }
+        None => Box::new(io::stdout()),
+    })
+}
+
+pub fn read_file_or_stdin(filename: Option<impl AsRef<Path>>) -> Result<Vec<u8>, io::Error> {
+    let mut reader = open_file_or_stdin(filename)?;
+    let mut buf = Vec::new();
+    reader.read_to_end(&mut buf)?;
+    Ok(buf)
+}
+
+pub fn write_file_or_stdout(
+    data: impl AsRef<[u8]>,
+    filename: Option<impl AsRef<Path>>,
+) -> Result<(), io::Error> {
+    let mut writer = open_file_or_stdout(filename)?;
+    writer.write_all(data.as_ref())
+}
