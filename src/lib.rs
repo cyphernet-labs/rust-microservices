b43dbaf1864e02764157e5b401f83580dd6d2795
Dr Maxim Orlovsky <orlovsky@cyphernet.io>
2025-04-07 12:18:53 +0200

    chore: re-start the project

--- src/lib.rs
+++ src/lib.rs
@@ -1,77 +1,28 @@
-// LNP/BP Core Library implementing LNPBP specifications & standards
-// Written in 2020 by
-//     Dr. Maxim Orlovsky <orlovsky@pandoracore.com>
+// Channel-based non-blocking microservices without use of async
 //
-// To the extent possible under law, the author(s) have dedicated all
-// copyright and related and neighboring rights to this software to
-// the public domain worldwide. This software is distributed without
-// any warranty.
+// SPDX-License-Identifier: Apache-2.0
 //
-// You should have received a copy of the MIT License
-// along with this software.
-// If not, see <https://opensource.org/licenses/MIT>.
+// Written in 2022-2025 by
+//     Dr. Maxim Orlovsky <orlovsky@cyphernet.org>
+//
+// Copyright (C) 2022-2025 Cyphernet Labs, 
+//                         Institute for Distributed and Cognitive Systems, 
+//                         Lugano, Switzerland
+// All rights reserved
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-//! LNP/BP library for building robust and scalable APIs, servers, nodes,
-//! clients and cli utilities with LNP protocol, ZMQ and multi-threading
-
-#![recursion_limit = "256"]
-// Coding conventions
-#![deny(
-    non_upper_case_globals,
-    non_camel_case_types,
-    non_snake_case,
-    unused_mut,
-    dead_code,
-    //missing_docs
-)]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
 
 #[macro_use]
 extern crate amplify;
-#[macro_use]
-extern crate strict_encoding;
-
-#[cfg(feature = "serde")]
-#[allow(unused_imports)]
-#[macro_use]
-extern crate serde_crate as serde;
-#[cfg(feature = "serde")]
-#[cfg_attr(feature = "shell", macro_use)]
-extern crate serde_with;
-
-#[cfg(feature = "clap")]
-#[macro_use]
-extern crate clap;
-#[cfg_attr(feature = "_rpc", macro_use)]
-extern crate log;
-#[cfg(feature = "zmq")]
-extern crate zmq2 as zmq;
-
-#[cfg(feature = "cli")]
-pub mod cli;
-pub mod error;
-#[cfg(feature = "_rpc")]
-pub mod esb;
-#[cfg(feature = "shell")]
-mod format;
-#[cfg(feature = "node")]
-mod launcher;
-#[cfg(feature = "node")]
-pub mod node;
-#[cfg(feature = "peer")]
-pub mod peer;
-#[cfg(feature = "_rpc")]
-pub mod rpc;
-#[cfg(feature = "shell")]
-pub mod shell;
-pub mod util;
-
-#[cfg(feature = "zmq")]
-use once_cell::sync::Lazy;
-
-#[cfg(feature = "shell")]
-pub use crate::format::{BinaryFormat, FileFormat, FormatParseError, StructuredFormat};
-#[cfg(feature = "node")]
-pub use crate::launcher::{DaemonHandle, Launcher, LauncherError};
-
-#[cfg(feature = "zmq")]
-pub static ZMQ_CONTEXT: Lazy<zmq::Context> = Lazy::new(zmq::Context::new);
